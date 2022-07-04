43a17c4551cd7692266f7186cc92d2ae102d0a1c
soliditee <108051250+soliditee@users.noreply.github.com>
2022-07-03 21:44:58 -0700

    Deploy to testnet and auto-verify contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle")
 require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -11,6 +12,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     }
 })
 
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -20,4 +25,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
     defaultNetwork: "hardhat",
     solidity: "0.8.7",
+    networks: {
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,5 @@
 // Short for: const ethers = require("hardhat").ethers;
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 async function main() {
     const SimpleStorageFactory = await ethers.getContractFactory(
@@ -8,6 +8,36 @@ async function main() {
 
     const simpleStorage = await SimpleStorageFactory.deploy()
     console.log(`Deployed to ${simpleStorage.address}`)
+
+    if (network.config.chainId === 4) {
+        //Only verify if network is Rinkeby
+        await simpleStorage.deployTransaction.wait(6)
+        await verify(simpleStorage.address, [])
+    }
+
+    // Using the new contract
+    const currentNumber = await simpleStorage.getFavoriteNumber("jon")
+    console.log(`Current favorite number of jon: ${currentNumber}`)
+    const transactionResponse = await simpleStorage.addPerson("jon", "7")
+    await transactionResponse.wait(1)
+    const updatedNumber = await simpleStorage.getFavoriteNumber("jon")
+    console.log(`Updated favorite number of jon: ${updatedNumber}`)
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArgsParams: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Contract already verified")
+        } else {
+            console.log(e)
+        }
+    }
 }
 
 main()
